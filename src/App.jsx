e83ffb0f4c5eb02df5fa8e89a4ee9e3aad86e16a
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-07-08 17:25:12 +0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Card</h1>
+          <Card/>
+        </header>
+      </div>
+  );
+}
+
+export default App;
