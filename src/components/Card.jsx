18ce2f36449a2d77d31b91babb00ddde76001fb2
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-07-08 17:34:32 +0300

    react_HW_1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
 class Card extends React.Component {
-    render() {
-                return (
-                    <div className="card" style={{ width: '300px', margin: '20px' }}>
-                        <div className="card-body">
-                            <h5 className="card-title">Card Title</h5>
-                            <p className="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the card's content.
-                            </p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                );
-    }
+    render(){
+        return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card</p>
+                <button type="button" className="btn btn-primary">Go somewhere</button>
+            </div>
+        </div>
+ );
+}
 }
 
 export default Card;
