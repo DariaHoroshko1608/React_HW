edb965c53d57c872ad67df124363e1576e112a0c
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-07-20 16:00:25 +0300

    react_81

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Progress from "./components/Progress";
+import Alert from "./components/Alert";
 
 function App() {
     return (
         <div>
-            <h1>Прогресс:</h1>
-            <Progress percentage={40} />
+            <h1>Bootstrap Alert Example</h1>
+            <Alert type="warning" text="What is love?" />
         </div>
     );
 }
---
+++ src/components/Alert.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class Alert extends React.Component {
+    render() {
+        const { type, text } = this.props;
+        const alertClass = `alert alert-${type}`;
+
+        return (
+            <div className={alertClass} role="alert">
+                {text}
+            </div>
+        );
+    }
+}
+
+Alert.propTypes = {
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf([
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark'
+    ]).isRequired,
+};
+
+export default Alert;
